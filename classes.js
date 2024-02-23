d0da4dae9432148d52a5e2d57d2ef34316038786
0mariapaula <0mariapaulaferreira@gmail.com>
2024-02-23 11:07:36 -0300

    23 Fev (Voltando a como era antes)

--- classes.js
+++
@@ -1,8 +0,0 @@
-class Campeao {
-    constructor(id, nome, biografia, foto) {
-        this.id = id;
-        this.nome = nome;
-        this.biografia = biografia;
-        this.foto = foto;
-    }
-}
